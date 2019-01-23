6c8ab5419c2c82550202197cbc0b4b4cabbd58a8
jamiejamiebobamie <jmccrory@vt.edu>
2019-01-23 11:45:49 -0800

    completed all challenges for class 5

--- js/Event.js
+++ js/Event.js
@@ -58,27 +58,128 @@ OregonH.Event.eventTypes = [
     stat: 'oxen',
     value: 1,
     text: 'Found wild oxen. New oxen: '
-  }
+  },
+  {
+    type: 'SHOP',
+    notification: 'neutral',
+    text: 'You have found a shop',
+    products: [
+      {item: 'food', qty: 20, price: 50},
+      {item: 'oxen', qty: 1, price: 200},
+      {item: 'firepower', qty: 2, price: 50},
+      {item: 'crew', qty: 5, price: 80}
+    ]
+  },
+  {
+    type: 'SHOP',
+    notification: 'neutral',
+    text: 'You have found a shop',
+    products: [
+      {item: 'food', qty: 30, price: 50},
+      {item: 'oxen', qty: 1, price: 200},
+      {item: 'firepower', qty: 2, price: 20},
+      {item: 'crew', qty: 10, price: 80}
+    ]
+  },
+  {
+    type: 'SHOP',
+    notification: 'neutral',
+    text: 'Smugglers sell various goods',
+    products: [
+      {item: 'food', qty: 20, price: 60},
+      {item: 'oxen', qty: 1, price: 300},
+      {item: 'firepower', qty: 2, price: 80},
+      {item: 'crew', qty: 5, price: 60}
+    ]
+  },
+  {
+      type: 'ATTACK',
+      notification: 'negative',
+      text: 'Bandits are attacking you'
+    },
+    {
+      type: 'ATTACK',
+      notification: 'negative',
+      text: 'Bandits are attacking you'
+    },
+    {
+      type: 'ATTACK',
+      notification: 'negative',
+      text: 'Bandits are attacking you'
+    }
+  ];
+  OregonH.Event.generateEvent = function(){
+    //pick random one
+    var eventIndex = Math.floor(Math.random() * this.eventTypes.length);
+    var eventData = this.eventTypes[eventIndex];
 
-];
+    //events that consist in updating a stat
+    if(eventData.type == 'STAT-CHANGE') {
+      this.stateChangeEvent(eventData);
+    }
 
-OregonH.Event.generateEvent = function(){
-  //pick random one
-  var eventIndex = Math.floor(Math.random() * this.eventTypes.length);
-  var eventData = this.eventTypes[eventIndex];
+    //shops
+    else if(eventData.type == 'SHOP') {
+      //pause game
+      this.game.pauseJourney();
 
-  //events that consist in updating a stat
-  if(eventData.type == 'STAT-CHANGE') {
-    this.stateChangeEvent(eventData);
-  }
+      //notify user
+      this.ui.notify(eventData.text, eventData.notification);
 
+      //prepare event
+      this.shopEvent(eventData);
+    }
 
-};
+    //attacks
+    else if(eventData.type == 'ATTACK') {
+      //pause game
+      this.game.pauseJourney();
 
-OregonH.Event.stateChangeEvent = function(eventData) {
-  //can't have negative quantities
-  if(eventData.value + this.caravan[eventData.stat] &gt;= 0) {
-    this.caravan[eventData.stat] += eventData.value;
-    this.ui.notify(eventData.text + Math.abs(eventData.value), eventData.notification);
-  }
-};
+      //notify user
+      this.ui.notify(eventData.text, eventData.notification);
+
+      //prepare event
+      this.attackEvent(eventData);
+    }
+  };
+
+  OregonH.Event.stateChangeEvent = function(eventData) {
+    //can't have negative quantities
+    if(eventData.value + this.caravan[eventData.stat] >= 0) {
+      this.caravan[eventData.stat] += eventData.value;
+      this.ui.notify(eventData.text + Math.abs(eventData.value), eventData.notification);
+    }
+  };
+
+  OregonH.Event.shopEvent = function(eventData) {
+    //number of products for sale
+    var numProds = Math.ceil(Math.random() * 4);
+
+    //product list
+    var products = [];
+    var j, priceFactor;
+
+    for(var i = 0; i < numProds; i++) {
+      //random product
+      j = Math.floor(Math.random() * eventData.products.length);
+
+      //multiply price by random factor +-30%
+      priceFactor = 0.7 + 0.6 * Math.random();
+
+      products.push({
+        item: eventData.products[j].item,
+        qty: eventData.products[j].qty,
+        price: Math.round(eventData.products[j].price * priceFactor)
+      });
+    }
+
+    this.ui.showShop(products);
+  };
+
+  //prepare an attack event
+  OregonH.Event.attackEvent = function(eventData){
+    var firepower = Math.round((0.7 + 0.6 * Math.random()) * OregonH.ENEMY_FIREPOWER_AVG);
+    var gold = Math.round((0.7 + 0.6 * Math.random()) * OregonH.ENEMY_GOLD_AVG);
+
+    this.ui.showAttack(firepower, gold);
+  };
